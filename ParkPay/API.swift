bb4915e3fe95ecb67411b126129ee280ab22998e
Jay Ravaliya <jayrav13@gmail.com>
2016-01-17 00:11:29 -0500

    Completed first screen

--- ParkPay/API.swift
+++ ParkPay/API.swift
@@ -7,7 +7,27 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import CoreLocation
 
 class API {
+    
+    static var baseURL : String = "http://koolaid.ngrok.io/"
+    
+    static func getNearbyParkingLocations(latitude : CLLocationDegrees, longitude : CLLocationDegrees, completion : (success : Bool, data : JSON) -> Void) -> Void {
+        
+        Alamofire.request(Method.GET, self.baseURL + "parking").responseJSON { (response) -> Void in
+            
+            if response.result.isSuccess {
+                completion(success: true, data: JSON(response.result.value!))
+            }
+            else {
+                completion(success: false, data: nil)
+            }
+            
+        }
+        
+    }
     
 }
--- ParkPay/ViewController.swift
+++ ParkPay/ViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 import CoreLocation
+import MapKit
+import SwiftyJSON
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
     var locationManager : CLLocationManager!
     var settingsBarButtonItem : UIBarButtonItem!
-    var streetNameLabel : UILabel!
+    
+    var openSpotsLabel : UILabel!
+    var payRateLabel : UILabel!
+    
+    var parkButton : UIButton!
+    
+    var mapView : MKMapView!
+    var allPins : [MKPointAnnotation]!
+    
+    var locationData : JSON!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        self.view.backgroundColor = UIColor.whiteColor()
+        self.view.backgroundColor = UIColor(red: 250/255, green: 250/255, blue: 250/255, alpha: 1)
+        self.title = "Select a Spot"
         
         self.locationManager = CLLocationManager()
         self.locationManager.delegate = self
@@ -30,7 +42,37 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         self.settingsBarButtonItem = UIBarButtonItem(title: "Settings", style: UIBarButtonItemStyle.Plain, target: self, action: "pushToSettingsViewController:")
         self.navigationItem.rightBarButtonItem = settingsBarButtonItem
         
-        // self.streetNameLabel = UILabel(frame: CGRect(x: Standard.screenHeight, y: <#T##Int#>, width: <#T##Int#>, height: <#T##Int#>))
+        self.mapView = MKMapView()
+        self.mapView.frame = CGRect(x: 0, y: Standard.screenHeight * 0, width: Standard.screenWidth, height: Standard.screenHeight * 0.5)
+        self.mapView.mapType = MKMapType.Standard
+        self.mapView.zoomEnabled = true
+        self.mapView.scrollEnabled = true
+        self.mapView.delegate = self
+        self.populateMap()
+        self.setMapCenter()
+        self.view.addSubview(mapView)
+        
+        self.openSpotsLabel = UILabel(frame: CGRect(x: 0, y: Standard.screenHeight * 0.50, width: Standard.screenWidth / 2, height: Standard.screenHeight * 0.05))
+        self.openSpotsLabel.text = "...open spots..."
+        self.openSpotsLabel.textAlignment = NSTextAlignment.Center
+        self.view.addSubview(self.openSpotsLabel)
+        
+        self.payRateLabel = UILabel(frame: CGRect(x: Standard.screenWidth / 2, y: Standard.screenHeight * 0.50, width: Standard.screenWidth / 2, height: Standard.screenHeight * 0.05))
+        self.payRateLabel.text = "...pay rate ..."
+        self.payRateLabel.textAlignment = NSTextAlignment.Center
+        self.view.addSubview(self.payRateLabel)
+        
+        self.parkButton = UIButton(type: UIButtonType.System)
+        self.parkButton.frame = CGRect(x: Standard.screenWidth * 0.35, y: Standard.screenHeight * 0.60, width: Standard.screenWidth * 0.30, height: Standard.screenHeight * 0.075)
+        self.parkButton.layer.cornerRadius = 10
+        self.parkButton.clipsToBounds = true
+        self.parkButton.setTitle("PARK", forState: UIControlState.Normal)
+        self.parkButton.addTarget(self, action: "parkButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.parkButton.backgroundColor = UIColor(red: 63/255, green: 81/255, blue: 181/255, alpha: 1)
+        self.parkButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        
+        self.parkButton.titleLabel?.font = UIFont(name: "Helvetica Neue", size: 28)
+        self.view.addSubview(self.parkButton)
         
     }
 
@@ -40,7 +82,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        print((manager.location?.coordinate.latitude)!)
+        // print((manager.location?.coordinate.latitude)!)
+    }
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        self.setMapCenter()
     }
     
     func pushToSettingsViewController(sender : UIButton) {
@@ -48,6 +94,37 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         self.navigationController?.pushViewController(settingsViewController, animated: true)
     }
 
+    func setMapCenter() {
+        let coordinateRegion : MKCoordinateRegion = MKCoordinateRegion(center: (self.locationManager.location?.coordinate)!, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+        self.mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func populateMap() {
+        API.getNearbyParkingLocations((self.locationManager.location?.coordinate.latitude)!, longitude: (self.locationManager.location?.coordinate.longitude)!) { (success, data) -> Void in
+            if success {
+                self.locationData = data
+                print(data)
+                for(var i = 0; i < data["parking"].count; i++) {
+                    let locationPin = MKPointAnnotation()
+                    locationPin.coordinate = CLLocationCoordinate2DMake(CLLocationDegrees(data["parking"][i]["latitude"].doubleValue), CLLocationDegrees(data["parking"][i]["longitude"].doubleValue))
+                    locationPin.title = data["parking"][i]["street"].stringValue
+                    self.mapView.addAnnotation(locationPin)
+                }
+                
+            }
+            else {
+                // Error message
+            }
+        }
+    }
+    
+    func parkButtonPressed(sender : UIButton) {
+        
+    }
+    
+    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
+        self.title = mapView.selectedAnnotations[0].title!
+    }
 
 }
 
